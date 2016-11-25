4e0d5a9afc8f11eafb08d56ef130cc13e8eda5e9
Laukkanen Suvi <a1500951@myy.haaga-helia.fi>
2016-11-25 13:54:40 +0200

    dao

--- src/dao/Dao.java
+++ src/dao/Dao.java
@@ -11,8 +11,15 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.sql.Statement;
 
+import pizzicato.model.Pizza;
+import pizzicato.model.dao.Exception;
+import pizzicato.model.dao.RuntimeException;
+import pizzicato.model.dao.Statement;
+import pizzicato.model.dao.String;
 import kohdeluokat.Asiakas;
+import kohdeluokat.Lainaus;
 import kohdeluokat.PostinumeroAlue;
 
 
@@ -46,6 +53,64 @@ public class Dao {
 			throw (new SQLException("Tietokanta-ajuria ei loydy!"));
 		}
 		return tietokantayhteys;
+	}
+	
+	protected static void close(ResultSet rs, Statement stmt, Connection conn) {
+		
+		try {
+			if (rs !=null) {
+				rs.close();
+			}
+			if (stmt !=null) {
+				stmt.close();
+			}
+			
+			if (conn !=null) {
+				conn.close();
+			}
+		} catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	protected static void close(Statement stmt, Connection connection) {
+		close (null, stmt, connection);
+	}
+	
+	private Lainaus read(ResultSet rs) {
+		try {
+			int numero = rs.getInt("numero");
+			Date lainausPvm = rs.getDate("lainauspvm");
+			int asiakasNro = rs.getInt("asiakasnro");
+			return new Lainaus(numero, lainausPvm, asiakasNro);
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public ArrayList <Lainaus> haeKaikki(){
+		Connection yhteys = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		ArrayList<Lainaus> lainat = new ArrayList<Lainaus>();
+		Lainaus lainaus = null;
+		
+		try{
+			yhteys = yhdista();
+			String sqlSelect = "";
+			stmt = yhteys.prepareStatement(sqlSelect);
+			rs=stmt.executeQuery(sqlSelect);
+			
+		}catch(SQLException e){
+			throw new RuntimeException(e);
+		}finally{
+			close(rs,stmt,yhteys);
+		}
+		
+		
+		
+		return lainat;
+		
 	}
 	
 }
