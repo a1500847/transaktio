a65f877dc69ced5bc90b15c6f34cfb4266d06f51
Laihonen Mindi <a1500847@myy.haaga-helia.fi>
2016-11-28 15:49:16 +0200

    lainaus servlet

--- src/ohjelma/LainausServlet.java
+++ src/ohjelma/LainausServlet.java
@@ -1,25 +1,41 @@
 package ohjelma;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import kohdeluokat.Lainaus;
+import dao.Dao;
+
 
 @WebServlet("/LainausServlet")
 public class LainausServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-  
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		Dao dao = new Dao();
+		ArrayList<Integer> lainausNumerot = dao.haeLainausNrot();
+		request.setAttribute("lainausnumerot", lainausNumerot);
+		
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/WEB-INF/etusivu.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String nroString = request.getParameter("lainausnumero");
+		int lainausnro = new Integer(nroString);
+		Dao dao = new Dao();
+		Lainaus lainaus = dao.hae(lainausnro);
+		request.setAttribute("lainaus", lainaus);
+		
+		response.sendRedirect("YksiLainausServlet");
 	}
 
 }
